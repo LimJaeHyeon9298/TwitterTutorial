f6100ed68e354adcbbe326f7b46ce8fbc43cd1d7
LimJaeHyeon9298 <limjh9298@gmail.com>
2023-09-15 19:35:34 +0900

    MainTabController 생성 및 sceneDelegate 설정

---
+++ TwitterTutorial/Controller/MainTabController.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabController.swift
+//  TwitterTutorial
+//
+//  Created by 임재현 on 2023/09/15.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+
+
+}
