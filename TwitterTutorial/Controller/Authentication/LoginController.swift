ea64e3c324882df8666cd622ea7130d320e68165
LimJaeHyeon9298 <limjh9298@gmail.com>
2023-09-15 22:35:54 +0900

    Login 버튼, Signup 버튼 구현

--- TwitterTutorial/Controller/Authentication/LoginController.swift
+++ TwitterTutorial/Controller/Authentication/LoginController.swift
@@ -21,21 +21,45 @@ class LoginController: UIViewController {
     
     private lazy var emailContainerView: UIView  = {
         let image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
-        let view = Utilities().inputContainerView(withImage: image)
+        let view = Utilities().inputContainerView(withImage: image, textField: emailTextField)
         return view
     }()
     
     private lazy var passwordContainerView: UIView  = {
         let image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
-        let view = Utilities().inputContainerView(withImage: image)
+        let view = Utilities().inputContainerView(withImage: image, textField: passwordTextField)
         return view
     }()
     
+    private let emailTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Email")
+        return tf
+    }()
     
+    private let passwordTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Password")
+        tf.isSecureTextEntry = true
+        return tf
+       
+    }()
     
+    private let loginButton:UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.setTitleColor(.twitterBlue, for: .normal)
+        button.backgroundColor = .white
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
     
-    
-    
+    private let dontHaveAccountButton:UIButton = {
+        let button = Utilities().attributedButton("Dont have an account", " Sign Up")
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        return button
+    }()
     
     
     //MARK: - Lifecycle
@@ -46,6 +70,13 @@ class LoginController: UIViewController {
     }
     //MARK: - Selector
     
+    @objc func handleLogin() {
+        print(123)
+    }
+    
+    @objc func handleShowSignUp() {
+        print(1234)
+    }
     //MARK: - Helpers
     func configUI() {
         view.backgroundColor = .twitterBlue
@@ -56,12 +87,16 @@ class LoginController: UIViewController {
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
         logoImageView.setDimensions(width: 150, height: 150)
         
-        let stack = UIStackView(arrangedSubviews: [emailContainerView,passwordContainerView])
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,passwordContainerView,loginButton])
         stack.axis = .vertical
-        stack.spacing = 8
+        stack.spacing = 20
+        stack.distribution = .fillEqually
         
         view.addSubview(stack)
-        stack.anchor(top: logoImageView.bottomAnchor,left: view.leftAnchor,right: view.rightAnchor)
+        stack.anchor(top: logoImageView.bottomAnchor,left: view.leftAnchor,right: view.rightAnchor,paddingLeft: 32,paddingRight: 32)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.anchor(left: view.leftAnchor,bottom: view.safeAreaLayoutGuide.bottomAnchor,right: view.rightAnchor,paddingLeft: 40,paddingRight: 40)
     }
     
 }
