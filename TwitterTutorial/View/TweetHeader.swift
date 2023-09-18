efb71434a1060096350d72450a6b9f03840aa900
LimJaeHyeon9298 <limjh9298@gmail.com>
2023-09-19 01:44:53 +0900

    Notification ViewModel 설정

--- TwitterTutorial/View/TweetHeader.swift
+++ TwitterTutorial/View/TweetHeader.swift
@@ -214,6 +214,8 @@ class TweetHeader: UICollectionReusableView {
         dateLabel.text = viewModel.headerTimestamp
         retweetsLabel.attributedText = viewModel.retweetsAttributedString
         likesLabel.attributedText = viewModel.retweetsAttributedString
+        likeButton.setImage(viewModel.likeButtonImage, for: .normal)
+        likeButton.tintColor = viewModel.likeButtonTintColor
     }
     
     
