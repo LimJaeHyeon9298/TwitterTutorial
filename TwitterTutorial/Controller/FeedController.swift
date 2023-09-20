493f0550ae75437707c25029db414c01c443e442
LimJaeHyeon9298 <limjh9298@gmail.com>
2023-09-21 00:36:44 +0900

    버그 수정 및 feed 정렬

--- TwitterTutorial/Controller/FeedController.swift
+++ TwitterTutorial/Controller/FeedController.swift
@@ -32,29 +32,44 @@ class FeedController: UICollectionViewController {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.isHidden = false
         navigationController?.navigationBar.barStyle = .default
+        print ("11111111")
     }
   
     override func viewDidLoad() {
         super.viewDidLoad()
         configUI()
         fetchTweets()
+        print("@222")
     }
+    //MARK: - Selecctors
+    
+    @objc func handleRefresh() {
+        fetchTweets()
+    }
+    
+    
+    
     
     //MARK: - API
     
     func fetchTweets() {
+        collectionView.refreshControl?.beginRefreshing()
         TweetService.shared.fetchTweets { tweets in
-            self.tweets = tweets
-            self.checkIfUserLikedTweets(tweets)
+            self.tweets =  tweets.sorted(by: { $0.timestamp > $1.timestamp })
+            self.checkIfUserLikedTweets()
+            
+            self.collectionView.refreshControl?.endRefreshing()
             
         }
     }
     
-    func checkIfUserLikedTweets(_ tweets:[Tweet]) {
-        for (index,tweet) in tweets.enumerated() {
+    func checkIfUserLikedTweets() {
+        tweets.forEach { tweet in
             TweetService.shared.checkIfUserLikedTweet(tweet) { didLike in
                 guard didLike == true else {return}
-                self.tweets[index].didLike = true
+                if let index = self.tweets.firstIndex(where: { $0.tweetID == tweet.tweetID }) {
+                    self.tweets[index].didLike = true
+                }
             }
         }
     }
@@ -70,6 +85,10 @@ class FeedController: UICollectionViewController {
         imageView.contentMode = .scaleAspectFit
         imageView.setDimensions(width: 44, height: 44)
         navigationItem.titleView = imageView
+        
+        let refresehControl = UIRefreshControl()
+        refresehControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        collectionView.refreshControl = refresehControl
         
        
     }
